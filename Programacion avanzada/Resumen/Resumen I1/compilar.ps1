64f1c898e522c580223255ea7ef07bbc42c0af8d
santiagofranulic <santiago.franulic@uc.cl>
2026-03-23 15:19:37 -0300

    23-03-2026 -- 15:19

---
+++ Programacion avanzada/Resumen/Resumen I1/compilar.ps1
@@ -0,0 +1,13 @@
+﻿$file = "Resumen_I1_Programacion_Avanzada.tex"
+$pdf = "Resumen_I1_Programacion_Avanzada.pdf"
+
+Write-Host "Compilando $file..."
+& "C:\Users\santi\AppData\Local\Programs\MiKTeX\miktex\bin\x64\pdflatex.exe" -interaction=nonstopmode $file
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Compilacion exitosa. Abriendo PDF..."
+    Start-Process $pdf
+} else {
+    Write-Host "Error en la compilacion."
+}
+
